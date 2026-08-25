c0302e2ab8d6870b144b05cf47c644589b19fac5
rishav-ghosh-98 <rishavghosh0798@gmail.com>
2026-08-25 19:01:12 +0530

    addded code splitting in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: true,
+        manualChunks(id) {
+          if (id.includes("node_modules/react/") || id.includes("node_modules/react-dom/") || id.includes("node_modules/react-router-dom/")) {
+            return "vendor-react";
+          }
+          if (id.includes("node_modules/chart.js/") || id.includes("node_modules/react-chartjs-2/")) {
+            return "vendor-charts";
+          }
+          if (id.includes("node_modules/bootstrap/") || id.includes("node_modules/react-icons/")) {
+            return "vendor-ui";
+          }
+        },
+      },
+    },
+  },
 })
